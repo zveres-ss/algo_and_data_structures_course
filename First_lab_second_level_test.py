b06ce452cd6aa8683c7df6a98b238993ac71d534
Vitaliy <69012012+Starik1500@users.noreply.github.com>
2024-03-01 12:12:09 +0200

    перейменовані файли, би можна було іспортувати функцію з іншого файлу в тестовий

---
+++ First_lab_second_level_test.py
@@ -0,0 +1,11 @@
+import unittest
+from First_lab_second_level import sort
+from First_lab_second_level import k_founder
+
+class TestCode(unittest.TestCase):
+    def test_sort(self):
+        self.assertEqual("Знайдений 3-й за величиною елемент: 22", "Знайдений 3-й за величиною елемент: 22")
+        self.assertEqual("Позиція 3-го найбільшого елемента: 6", "Позиція 3-го найбільшого елемента: 6")
+
+if __name__ == "__main__":
+    unittest.main()
