e147e3df2901d4389288d8ecc50a4bf5bed3235e
Vitaliy <69012012+Starik1500@users.noreply.github.com>
2024-03-08 12:16:37 +0200

    Виправив працездатність коду.py
    Виправив функцію check_placing_cows, щоби коли функція доходить до значення C, припинило пошук

--- Second_lab_third_level.py
+++ Second_lab_third_level.py
@@ -1,29 +1,30 @@
+N = 5
+
 def check_sections(N):
     if N >= 100000:
         print("Ви не мільйонер, не брешіть")
     elif N <= 2:
         print("Секцій, не секція, щоби вирішити данну проблему вам потрібно 3 секції")
 
-
-N = 5
 check_sections(N)
 
 C = 3
 
-
 def check_numbers_of_cows(C, N):
     if C >= N:
-        print(
-            "Якби у вас корів було більше секцій, ви би в любому випадку не вирішили би проблему))"
-        )
+        print("Якби у вас корів було більше секцій, ви би в любому випадку не вирішили би проблему))")
     if C <= 2:
         print("Хіба на м'ясо пускайте")
-
 
 check_numbers_of_cows(C, N)
 
 free_sections = [1, 2, 8, 4, 9]
 
+def sort(first, free_sections, last):
+    if first < last:
+        sort_partition_pos = sort_partition(first, free_sections, last)
+        sort(first, free_sections, sort_partition_pos - 1)
+        sort(sort_partition_pos + 1, free_sections, last)
 
 def sort_partition(first, free_sections, last):
     i = first
@@ -44,43 +45,36 @@ def sort_partition(first, free_sections, last):
 
     return i
 
-
-def sort(first, free_sections, last):
-    if first < last:
-        sort_partition_pos = sort_partition(first, free_sections, last)
-        sort(first, free_sections, sort_partition_pos - 1)
-        sort(sort_partition_pos + 1, free_sections, last)
-
-
 sort(0, free_sections, len(free_sections) - 1)
 
+def check_placing_cows(free_sections, min_distance, happy_cows):
+    F = len(free_sections)
+    happy_cows = 1
+    last_c = free_sections[0]
 
-def rescue_cows(N, C, free_sections):
-    def check_placing_cows(min_distance):
-        happy_cows = 1
-        last_p = free_sections[0]
+    for i in range(1, F):
+        if free_sections[i] - last_c >= min_distance:
+            happy_cows += 1
+            last_c = free_sections[i]
+        else:
+            continue
+        if happy_cows == C:
+            return True
+    return False
 
-        for section in free_sections[1:]:
-            if section - last_p >= min_distance:
-                happy_cows += 1
-
-                if happy_cows == C:
-                    return True
-        return False
-
-    left = 0
-    right = free_sections[-1] - free_sections[0]
-    result = -1
+def search(C, F):
+    left = 1
+    right = free_sections[F - 1] - free_sections[0]
 
     while left <= right:
         mid = (left + right) // 2
-        if check_placing_cows(mid):
-            result = mid
+        if check_placing_cows(free_sections, mid, C):
             left = mid + 1
         else:
             right = mid - 1
 
-        return result
+    return right
 
+b_search = search(free_sections, C)
 
-print(rescue_cows(N, C, free_sections))
+print(b_search)
